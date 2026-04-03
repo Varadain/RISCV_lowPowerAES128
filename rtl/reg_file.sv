23f9c2d0fe83e439c5dc73badd6abcf2af993884
Varadain <117143445+Varadain@users.noreply.github.com>
2026-04-03 17:44:15 +0530

    Update reg_file.sv

--- rtl/reg_file.sv
+++ rtl/reg_file.sv
@@ -17,31 +17,14 @@ module reg_file (
         end
     end
 
-    assign rs1_data_o = (rs1_i == 5'h0) ? 32'h0 : regs[rs1_i];
-    assign rs2_data_o = (rs2_i == 5'h0) ? 32'h0 : regs[rs2_i];
+assign rs1_bypass = rd_we_i && (rd_i != 5'h0) && (rd_i == rs1_i);
+assign rs2_bypass = rd_we_i && (rd_i != 5'h0) && (rd_i == rs2_i);
+assign rs1_data_o = (rs1_i == 5'h0) ? 32'h0 : (rs1_bypass ? rd_data_i : regs[rs1_i]);
+assign rs2_data_o = (rs2_i == 5'h0) ? 32'h0 : (rs2_bypass ? rd_data_i : regs[rs2_i]);
 
     always_ff @(posedge clk) begin
         if (rd_we_i && (rd_i != 5'h0)) begin
             regs[rd_i] <= rd_data_i;
         end
     end
-  input  logic        clk,
-  input  logic        we,
-  input  logic [4:0]  rs1,
-  input  logic [4:0]  rs2,
-  input  logic [4:0]  rd,
-  input  logic [31:0] wd,
-  output logic [31:0] rd1,
-  output logic [31:0] rd2
-);
-  logic [31:0] regs [0:31];
-
-  always_ff @(posedge clk) begin
-    if (we && (rd != 5'd0)) begin
-      regs[rd] <= wd;
-    end
-  end
-
-  assign rd1 = (rs1 == 5'd0) ? 32'h0 : regs[rs1];
-  assign rd2 = (rs2 == 5'd0) ? 32'h0 : regs[rs2];
 endmodule
