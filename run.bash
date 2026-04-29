b18940ecc649e74a39a2c6c607d684cfa24005e6
Varadain <117143445+Varadain@users.noreply.github.com>
2026-04-29 20:16:32 +0530

    Replace design.sv and testbench.sv with riscv_core files

---
+++ run.bash
@@ -0,0 +1,18 @@
+xrun -sysv -coverage all \
+  instr_mem.sv \
+  reg_file.sv \
+  imm_gen.sv \
+  control_unit.sv \
+  alu.sv \
+  load_store_unit.sv \
+  data_mem.sv \
+  pc_reg.sv \
+  if_stage.sv \
+  id_stage.sv \
+  ex_stage.sv \
+  mem_stage.sv \
+  wb_stage.sv \
+  forwarding_unit.sv \
+  hazard_unit.sv \
+  riscv_core_top.sv \
+  riscv_core_tb.sv
