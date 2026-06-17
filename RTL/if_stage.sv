b44c94858350fb6dd2e108023e02c8ddc2eaab29
Varadain <117143445+Varadain@users.noreply.github.com>
2026-06-17 17:03:02 +0530

    Delete RTL directory

--- RTL/if_stage.sv
+++
@@ -1,9 +0,0 @@
-module if_stage (
-    input  logic [31:0] pc_i,
-    output logic [31:0] instr_o
-);
-    instr_mem u_instr_mem (
-        .addr_i (pc_i),
-        .instr_o(instr_o)
-    );
-endmodule
--- RTL/wb_stage.sv
+++
@@ -1,8 +0,0 @@
-module wb_stage (
-    input  logic [31:0] alu_result_i,
-    input  logic [31:0] mem_read_data_i,
-    input  logic        mem_to_reg_i,
-    output logic [31:0] wb_data_o
-);
-    assign wb_data_o = mem_to_reg_i ? mem_read_data_i : alu_result_i;
-endmodule
